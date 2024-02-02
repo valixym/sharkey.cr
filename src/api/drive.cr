64d4bcd8981271373f006a02463500faa510b552
v <v@valixym.net>
2024-02-02 03:56:59 -0500

    some sleepy docs (nini)

--- src/api/drive.cr
+++ src/api/drive.cr
@@ -6,6 +6,15 @@ require "json"
 # of methods in this class require an API/auth key.
 class Sharkey::Drive
     # List the files in a user's drive.
+    #
+    # Request type: `POST`
+    # ## Example Usage of `drive/files`
+    # ```crystal
+    # # Note: in this example, I set an environment variable to my API key.
+    # # I recommend you do the same for convenience and safety.
+    # resp = Sharkey::Drive.files("https://kitsunes.club", ENV["SHARKEY_API_KEY"])
+    # puts resp
+    # ```
     def self.files(instance, bearerAuth, limit = 10, sort = "+createdAt")
         payload = {"limit" => limit, "sort" => sort}.to_json
         endpoint = "drive/files"
