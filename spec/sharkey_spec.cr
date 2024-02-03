1553a37695410be01394ee9e43f8175f2cba382c
v <v@valixym.net>
2024-02-03 18:50:13 -0500

    actually test that `notes/hybrid-timeline` works

--- spec/sharkey_spec.cr
+++ spec/sharkey_spec.cr
@@ -5,7 +5,7 @@ describe Sharkey do
   # TODO: Write tests
 
   it "works" do
-    resp = Sharkey::Drive.files_find("https://kitsunes.club", ENV["SHARKEY_API_KEY"], "valixym.png", folderId: "9pa68u9ttb")
+    resp = Sharkey::Notes.hybrid_timeline("https://kitsunes.club", ENV["SHARKEY_API_KEY"], withReplies: true, limit: 1)
     puts resp
   end
 end
